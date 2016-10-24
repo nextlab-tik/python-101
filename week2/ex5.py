79cb4fa44079c0206a10c88cd683eabb729ce0b6
firchtn <firaschaabencss@gmail.com>
2016-10-24 13:26:06 +0100

    add week2 examples

---
+++ week2/ex5.py
@@ -0,0 +1,15 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+# greation du class MyWindow
+class MyWindow(Gtk.Window):  # herité la class GTk.Window
+    def __init__(self): # constructeur du class Mywindow
+        Gtk.Window.__init__(self, title="window class") # appel au constructeur du class parent
+
+
+window = MyWindow() # generation d un objet du class Mywindow
+window.connect('delete-event', Gtk.main_quit)
+window.show_all()
+Gtk.main()
+
